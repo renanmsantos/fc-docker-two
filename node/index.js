0edf3c2e05aa3c8c225b7e6fdbac37dcbf994864
Renan Santos <renansantos@Renans-MacBook-Pro.local>
2023-07-26 15:49:01 -0300

    feat: first commit

---
+++ node/index.js
@@ -0,0 +1,34 @@
+const express =  require('express')
+const mysql = require('mysql')
+
+const app = express()
+const port = 3000
+
+const dbConfig = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'fullcycle'
+}
+const connection = mysql.createConnection(dbConfig)
+const sql = `INSERT INTO people(name) VALUES('Renan')`
+connection.query(sql)
+connection.end()
+
+app.get('/', (_,res) => {
+    let connection = mysql.createConnection(dbConfig)
+    connection.connect()
+    let title = "<h1>Full Cycle Rocks!</h1><br><br>";
+    connection.query('SELECT * FROM people', (err, row) => {
+        if (err) throw err;
+        row.forEach(element => {
+            title += "<p>NAME: "+ element.name +"</p>"
+        });
+        res.send(title);
+    })
+    connection.end()
+})
+
+app.listen(port, ()=> {
+    console.log("Running on port: " + port)
+})
